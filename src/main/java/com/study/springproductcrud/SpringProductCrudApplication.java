d3a33516579af25f5b01c59ca4cbc6802dc9c443
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-03-19 23:51:03 -0300

    first commit

---
+++ src/main/java/com/study/springproductcrud/SpringProductCrudApplication.java
@@ -0,0 +1,13 @@
+package com.study.springproductcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProductCrudApplication.class, args);
+    }
+
+}
