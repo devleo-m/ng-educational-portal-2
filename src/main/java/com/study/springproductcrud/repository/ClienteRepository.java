5785b6ad4eaec0c50b4109e78a8ae711c1c79fa5
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-07-26 04:01:43 -0300

    remove

--- src/main/java/com/study/springproductcrud/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.study.springproductcrud.repository;
-
-import com.study.springproductcrud.entites.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
-}
--- src/main/java/com/study/springproductcrud/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.study.springproductcrud.repository;
-
-import com.study.springproductcrud.entites.UsuarioEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
-}
